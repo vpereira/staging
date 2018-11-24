b4dc64fb670e72da97ceb8b9c6dc135ea956e6ad
Victor Pereira <vpereira@suse.de>
2018-11-24 14:41:30 +0100

    Handle apiurl as property

--- test_staged_request.py
+++ test_staged_request.py
@@ -4,8 +4,15 @@ from staged_request import StagedRequest
 
 
 class MockCommandLineObject(object):
+    def __init__(self, **args):
+        if 'apiurl' in args:
+            self._apiurl = args['apiurl']
+        else:
+            self._apiurl = 'http://frontend:3000'
+
+    @property
     def apiurl(self):
-        return 'http://frontend:3000'
+        return self._apiurl
 
     def mainproject(self):
         return "1"
@@ -17,11 +24,17 @@ class MockCommandLineObject(object):
         return "1"
 
 
-class TestStagedRequest(unittest.TestCase):
+class TestStagedRequestCreation(unittest.TestCase):
 
     def test_creation(self):
-        self.assertIsNotNone(StagedRequest('staged_requests',
-                                           MockCommandLineObject()))
+        sr = StagedRequest('staged_requests', MockCommandLineObject(**{}))
+        self.assertIsNotNone(sr)
+        self.assertEquals(sr.apiurl, 'http://frontend:3000')
+
+    def test_apiurl(self):
+        sr = StagedRequest('staged_requests', MockCommandLineObject(**{'apiurl': 'http://frontend1:3000'}))
+        self.assertIsNotNone(sr)
+        self.assertEquals(sr.apiurl, 'http://frontend1:3000')
 
     def test_wrong_command_raises(self):
         with self.assertRaises(oscerr.WrongArgs):
